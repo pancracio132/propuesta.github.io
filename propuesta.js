79922b085285b425eac1c3eaf94dd01f1cb8ca72
pancracio132 <rosalespancracio024@gmail.com>
2025-12-06 10:17:11 -0500

    Create propuesta.js

---
+++ propuesta.js
@@ -0,0 +1,4 @@
+document.getElementById("aceptarBtn").addEventListener("click", function(){
+    document.getElementById("extra").classList.remove("oculto");
+    alert("¡Yay! Si aceptas, serás la mejor profe de baile del mundo 😎💜");
+});
